89fba294932d09fc2b2daac0eddf44e437d95d63
Darren Rose <thorn086@gmail.com>
2019-08-05 23:14:12 -0500

    updated first fetch

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function displayResults(wgerJson) {
     for (let i = 0; i < wgerJson.suggestions.length; i++){
         
       $('#wger-results-user').append(
-        `<li><h3>${wgerJson.suggestion[i].data.name}</h3>
-        <p>${wgerJson.suggestion[i].data.category}</p>
-        <a href='${wgerJson.suggestion[i].data.id}'</a>
+        `<li><h3>${wgerJson.suggestions[i].data.name}</h3>
+        <p>${wgerJson.suggestions[i].data.category}</p>
+        <button class="id-fetch" data-id="${wgerJson.suggestions[i].data.id}">Learn More</button>
         </li>`
       )};
      
@@ -62,4 +62,29 @@ function handleSubmit() {
     getTerm(userTerm);
 });
 }
-$(handleSubmit);
+// id fetch display description and name
+function handleLearnMore(){
+    $('#wger-results-user').on('click','.id-fetch', event => {
+        const idName= $(event.target).attr('data-id')
+        console.log(idName);
+// call a function fetchExInfo (pass idName) {fetch https://wger.de/api/v2/exercise/74/}
+    })
+}
+$(function() {
+    handleSubmit();
+    handleLearnMore();
+});
+
+
+
+//hadle checked items
+
+var limit = 3;
+$('.compare_items').on('click', function(evt) {
+  index = $(this).parent('td').parent('tr').index();
+  if ($('.compare_items:checked').length >= limit) {
+    $('.compare_items').eq(localStorage.getItem('last-checked-item')).removeAttr('checked');
+    //this.checked = false;
+  }
+  localStorage.setItem('last-checked-item', index);
+});
