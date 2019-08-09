4f212d2b74af7fd33e8c009330a4e52505af3eda
Darren Rose <thorn086@gmail.com>
2019-08-09 13:01:50 -0500

    wger search funtions finalized

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ let lastSearchResult;
 
 
 
+
+
 //format params
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
@@ -17,11 +19,11 @@ function formatQueryParams(params) {
 function displayResults(wgerJson) {
 
     console.log(wgerJson);
-    
+
     $('#wger-results-user').empty();
 
     for (let i = 0; i < wgerJson.suggestions.length; i++) {
-        
+
         $('#wger-results-user').append(
             `<li><h3>${wgerJson.suggestions[i].data.name}</h3>
         <p>${wgerJson.suggestions[i].data.category}</p>
@@ -37,24 +39,50 @@ function displayResults(wgerJson) {
 
 // display id from handleLearnMore() to DOM
 function idResults(idJson) {
-    
+
     console.log(idJson);
-    $('#wger-results-user').empty();
-    
-        $('#wger-results-user').append(
-            `<li><h3>${idJson.name}</h3>
+    $('#wger-results-user').hide();
+
+    $('#wger-learnMore-user').append(
+        `<li><h3>${idJson.name}</h3>
         <p>${idJson.description}</p>
-        <button onclick="displayResults(lastSearchResult)"> Go Back</button>
+        <button onclick="goBack()"> Go Back</button>
         <button><a href="#startOver">Search Again</a></button></li>`
-        
-        );
-    
+
+    );
+
+
+    //$('#results').removeClass('hidden');
+};
+
+function goBack(){
+    $('#wger-results-user').show();
+    $('#wger-learnMore-user').empty();
+}
+
+// display results from search two to the DOM
+function radioResults(radioJson,radioLabel) {
+
+    console.log(radioJson);
+
+    $('#wger-results-user').empty();
+
+    for (let i = 0; i < radioJson.results.length; i++) {
+
+        $('#wger-results-user').append(
+            `<li><h3>${radioJson.results[i].name}</h3>
+        <p>${radioLabel}</p>
+        <button class="id-fetch" data-id="${radioJson.results[i].id}">Learn More</button>
+        </li>`
+        )
+    };
 
     //$('#results').removeClass('hidden');
 };
 
 
-// creat a term fetch 
+
+// create a term fetch(search one) 
 function getTerm(userTerm) {
     const params = {
         term: userTerm
@@ -64,7 +92,7 @@ function getTerm(userTerm) {
     const url = searchURL + '?' + queryString;
 
     console.log(url);
-   
+
     fetch(url)
         .then(wger => {
             if (wger.ok) {
@@ -74,16 +102,17 @@ function getTerm(userTerm) {
         })
         .then(wgerJson => {
             lastSearchResult = wgerJson;
-            displayResults(wgerJson)})
+            displayResults(wgerJson)
+        })
         .catch(err => {
             $('#js-error-message').text(`Opps!: ${err.message}`);
         });
 }
 
 
-// listener for the submit button to display the results
+// listener for the submit button(search one) to display the results
 function handleSubmit() {
-    $('#exercise-form').submit(event => {
+    $('#search-one').on('click', event => {
         event.preventDefault();
         const userTerm = $('#search-field').val();
         getTerm(userTerm);
@@ -94,45 +123,61 @@ function handleLearnMore() {
     $('#wger-results-user').on('click', '.id-fetch', event => {
         const idName = $(event.target).attr('data-id')
         console.log(idName);
-
-        function fetchExInfo(idName) {
-            const exURL = "https:/wger.de/api/v2/exercise/" + idName;
-
-            console.log(exURL);
-
-            fetch(exURL)
-                .then(id => {
-                    if (id.ok) {
-                        return id.json();
-                    }
-                    throw new Error(id.statusText);
-                })
-                .then(idJson => idResults(idJson))
-                .catch(err => {
-                    $('#js-error-message').text(`Opps!: ${err.message}`);
-                });
-        }
         fetchExInfo(idName);
     });
-
 }
 
-function checkButtonSearch(){
-    $('#search-two').on('click', event =>{
-    event.preventDefault();   
-    let radioValue = $("input[name='Body']:checked").val();
-    console.log(radioValue);
-});
+function fetchExInfo(idName) {
+    const exURL = "https:/wger.de/api/v2/exercise/" + idName;
+
+    console.log(exURL);
+
+    fetch(exURL)
+        .then(id => {
+            if (id.ok) {
+                return id.json();
+            }
+            throw new Error(id.statusText);
+        })
+        .then(idJson => idResults(idJson))
+        .catch(err => {
+            $('#js-error-message').text(`Opps!: ${err.message}`);
+        });
 }
-//
+
+//listener for search two
+function checkButtonSearch() {
+    $('#search-two').on('click', event => {
+        event.preventDefault();
+        let radioValue = $("input[name='Body']:checked").val();
+        const radioURL = "https://wger.de/api/v2/exercise/?category=" + radioValue + "&language=2&status=2";
+        const radioLabel = $(  $(":radio[name=Body]:checked").prop("labels") ).text();
+        console.log(radioLabel);
+        console.log(radioURL);
+
+        fetch(radioURL)
+            .then(radio => {
+                if (radio.ok) {
+                    return radio.json();
+                }
+                throw new Error(radi0.statusText);
+            })
+            .then(radioJson => radioResults(radioJson,radioLabel))
+            .catch(err => {
+                $('#js-error-message').text(`Opps!: ${err.message}`);
+            });
+    });
+}
+
+
 
 
 $(function () {
     handleSubmit();
     handleLearnMore();
     checkButtonSearch();
-    
-    
+
+
 });
 
 
